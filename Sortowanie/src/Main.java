4ba8968b2a84de35f2c9eb4d6ef71b5f3e2b47d5
marbaczy <mail@mbaczynski.pl>
2023-02-14 16:15:05 +0100

    bubblesort

--- Sortowanie/src/Main.java
+++ Sortowanie/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] random = {1,15,23,358,75,15,12,11,5,6,7,9,1,5,4,6,23,5,8,9,4,3,4,8,8,5,2,4,8,6};
+
+        System.out.println("Random: ");
+        printTab(random);
+        System.out.println("Sorted: ");
+        printTab(Sort.bubbleSort(random, true));
+        System.out.println("Reverse: ");
+        printTab(Sort.bubbleSort(random, false));
+
+    }
+
+    static void printTab(int[] tab)
+    {
+        for (int n : tab)
+        {
+            System.out.print(n+" ");
+        }
+        System.out.println();
     }
 }
--- Sortowanie/src/Sort.java
+++ Sortowanie/src/Sort.java
@@ -1,9 +1,34 @@
 public class Sort {
 
-    public static int[] bubbleSort(int[] collection)
+    public static int[] bubbleSort(int[] collection, boolean ascending)
     {
         int temp;
-        
+        int length = collection.length;
+
+        if (ascending) {
+            while (length > 1) {
+                for (int i = 0; i < length - 1; i++) {
+                    if (collection[i] > collection[i + 1]) {
+                        temp = collection[i];
+                        collection[i] = collection[i + 1];
+                        collection[i + 1] = temp;
+                    }
+                }
+                length--;
+            }
+        }else {
+            while (length > 1) {
+                for (int i = 0; i < length - 1; i++) {
+                    if (collection[i] < collection[i + 1]) {
+                        temp = collection[i];
+                        collection[i] = collection[i + 1];
+                        collection[i + 1] = temp;
+                    }
+                }
+                length--;
+            }
+        }
+
         return collection;
     }
 }
